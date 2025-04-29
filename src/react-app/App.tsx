6623b252049f1dcfbe75f94fb78e425d42bc8a15
lc <lichon@foxmail.com>
2025-04-30 04:19:29 +0800

    media ok checkpoint

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,33 +1,131 @@
 // src/App.tsx
 
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
+
+import { WHIPClient } from '@eyevinn/whip-web-client'
+import { WebRTCPlayer } from "@eyevinn/webrtc-player";
+
+async function newSession() {
+  const res = await fetch('/api/sessions/new', { method: 'POST' })
+  const json = await res.json() as { sessionId: string }
+  return json.sessionId
+}
 
 function App() {
-  const [name, setName] = useState("unknown");
+  const [session, setSession] = useState<string | null>(() => {
+    const params = new URLSearchParams(window.location.search)
+    return params.get('sid') || ''
+  })
+  const [whipClient, setWHIPClient] = useState<WHIPClient | null>()
+
+  async function joinSession(sid: string) {
+    const video = document.querySelector<HTMLVideoElement>('#video')
+    if (!video)
+      throw Error('no video element')
+
+    const player = new WebRTCPlayer({
+      video: video,
+      type: 'whep',
+      statsTypeFilter: '^candidate-*|^inbound-rtp',
+      debug: true,
+      iceServers: [{ urls: 'stun:stun.cloudflare.com:3478' }],
+    })
+    const url = new URL(window.location.href)
+    url.pathname = `api/session/${sid}/join`
+    url.search = ''
+    await player.load(url);
+    player.unmute()
+    return sid
+  }
+
+  async function deleteSession() {
+    if (whipClient) {
+      await whipClient.destroy()
+      setWHIPClient(null)
+      setSession(null)
+    }
+  }
+
+  async function createSession() {
+    const sid = await newSession()
+    const url = new URL(window.location.href)
+    const client = new WHIPClient({
+      endpoint: `${url}api/session/${sid}`,
+      opts: {
+        debug: true,
+        noTrickleIce: true,
+        iceServers: [{ urls: 'stun:stun.cloudflare.com:3478' }]
+      }
+    })
+    await client.setIceServersFromEndpoint()
+
+    const videoIngest = document.querySelector<HTMLVideoElement>('#video')
+    if (!videoIngest)
+      throw Error('video tag not found')
+
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    })
+    videoIngest.srcObject = mediaStream
+    await client.ingest(mediaStream)
+    setSession(sid)
+    setWHIPClient(client)
+  }
 
   return (
     <>
-      <div id="control" className="control">
-        <button className="control-bt"
-          onClick={() => setSession((count) => count + 1)}
-        >
-        </button>
-        <button className="control-bt"
-          onClick={() => {
-            fetch("/api/kv/hello")
-              .then((res) => res.text() as Promise<string>)
-              .then((data) => setName(data));
+      <div id='control' className='control'>
+        <button className='control-bt'
+          onClick={async (e) => {
+            const bt = e.target as HTMLButtonElement
+            if (bt.innerText == 'stop') {
+              deleteSession()
+              return
+            }
+            if (session)
+              joinSession(session)
+            else
+              createSession()
           }}
         >
-          Name from API is: {name}
+          {new URLSearchParams(window.location.search).get('sid') ? 'join' : session ? 'stop' : 'start'}
+        </button>
+        <button className='control-bt'
+          onClick={() => {
+            const url = new URL(window.location.href)
+            url.searchParams.set('sid', session || '')
+            navigator.clipboard.writeText(url.toString())
+              // .then(() => { window.open(url.toString(), '_blank') })
+              .catch((err) => {
+                console.error('Failed to copy:', err)
+                alert('Failed to copy link')
+              })
+          }}
+        >
+          Copy view link
+        </button>
+        <button className='control-bt'
+          onClick={async () => {
+            try {
+              const res = await fetch('/api/session/' + session)
+              if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`)
+              }
+            } catch (error) {
+              console.error('test failed:', error)
+            }
+          }}
+        >
+          Info
         </button>
       </div>
-      <div className="media">
-        <video id="video" autoPlay></video>
+      <div className='media'>
+        <video id='video' autoPlay muted></video>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,32 +1,443 @@
 import { Hono } from 'hono'
 
 type Bindings = {
-    KVASA: KVNamespace
+  KVASA: KVNamespace
+  ICE_URL: string
+  ICE_KEY: string
+  RTC_APP_ID: string
+  RTC_API_TOKEN: string
+}
+
+const RTC_URL = "https://rtc.live.cloudflare.com/v1/apps/811fa2b2719039f47b80ad3154dca458"
+
+interface NewSessionResponse {
+  sessionId: string
+}
+
+interface Track {
+  location: 'local' | 'remote'
+  mid?: string
+  trackName: string
+  sessionId?: string
+}
+
+interface SessionDescription {
+  sdp: string
+  type?: 'offer' | 'answer'
+}
+
+interface TracksResponse {
+  requiresImmediateRenegotiation?: boolean
+  tracks: Track[]
+  sessionDescription: SessionDescription
+}
+
+interface TracksRequest {
+  tracks: Track[]
+  sessionDescription?: SessionDescription
+  autoDiscover?: boolean
+}
+
+interface SessionStat {
+  tracks: Track[]
+  errorCode: string
+  errorDescription: string
+}
+
+// Add this helper function at the top of the file after the imports
+const rtcApi = (token: string, url: string, init?: RequestInit) => {
+  return fetch(RTC_URL + url, {
+    ...init,
+    headers: {
+      ...init?.headers,
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+  })
+}
+
+function createTracksRequest(sdp?: string, tracks?: Track[], sid?: string): TracksRequest {
+  if (tracks) {
+    const remoteTracks = tracks.map(t => {
+      return { location: 'remote', sessionId: sid, trackName: t.trackName } as Track
+    })
+    return {
+      tracks: remoteTracks,
+      ... (sdp?.length ?
+        {
+          sessionDescription: { sdp: sdp, type: 'offer' }
+        } : {}
+      )
+    }
+  }
+
+  return {
+    sessionDescription: {
+      sdp: sdp,
+      type: 'offer'
+    },
+    autoDiscover: true
+  } as TracksRequest
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/api/', (c) => c.json({ name: 'Cloudflare' }))
+app.get('/api/', (c) => c.json({ name: 'api' }))
 
-app.get('/api/kv/:key', async (c) => {
-    const key = c.req.param('key')
-    const v = await c.env.KVASA.get(key)
-    if (v) {
-        return c.text(v)
-    } else {
-        return c.text(`Key not found`)
-    }
+app.post('/api/kv/:key', async (c) => {
+  const key = c.req.param('key')
+  const value = await c.req.text()
+  await c.env.KVASA.put(key, value)
+  return c.text(`Key ${key} set to ${value}`)
 })
 
-app.put('/api/session/:sid', async (c) => {
-    const body = await c.req.json()
-    const sid = 'session_' + c.req.param('sid')
-    await c.env.KVASA.put(sid, body)
+app.get('/api/kv/:key', async (c) => {
+  const key = c.req.param('key')
+  const v = await c.env.KVASA.get(key)
+  if (v) {
+    return c.text(v)
+  } else {
+    return c.text(`Key not found`)
+  }
+})
+
+app.post('/api/sessions/new', async (c) => {
+  const res = await rtcApi(c.env.RTC_API_TOKEN, `/sessions/new`, { method: 'POST' })
+  const jsonRes = await res.json() as { sessionId: string }
+  return c.json(jsonRes)
+})
+
+app.post('/api/session/:sid', async (c) => {
+  const body = await c.req.text()
+  const sid = c.req.param('sid')
+  const res = await rtcApi(c.env.RTC_API_TOKEN, `/sessions/${sid}/tracks/new`, {
+    method: 'POST',
+    body: JSON.stringify(createTracksRequest(body))
+  }).catch(err => {
+    return c.text('Error: ' + err, 500)
+  })
+  const jsonResponse = await res.json() as TracksResponse
+  return c.text(jsonResponse.sessionDescription.sdp)
+})
+
+app.post('/api/session/:sid/join', async (c) => {
+  let sid = c.req.param('sid')
+  const playerSdp = await c.req.text()
+  const sessionStat = await getSessionTracks(c.env.RTC_API_TOKEN, sid)
+  const request = createTracksRequest(playerSdp, sessionStat.tracks, sid)
+
+  // use new session
+  const session = await newSession(c.env.RTC_API_TOKEN)
+  sid = session.sessionId
+
+  const res = await rtcApi(c.env.RTC_API_TOKEN, `/sessions/${sid}/tracks/new`, {
+    method: 'POST',
+    body: JSON.stringify(request)
+  })
+  const joinRes = await res.json() as TracksResponse
+  return c.text(joinRes.sessionDescription.sdp, 201)
 })
 
 app.get('/api/session/:sid', async (c) => {
-    const sid = 'session_' + c.req.param('sid')
-    return c.json(await c.env.KVASA.get(sid))
+  const sid = c.req.param('sid')
+  return c.json(await getSessionTracks(c.env.RTC_API_TOKEN, sid))
 })
+
+async function newSession(token: string) {
+  const res = await rtcApi(token, `/sessions/new`, { method: 'POST' })
+  return await res.json() as NewSessionResponse
+}
+
+async function getSessionTracks(token: string, sid: string) {
+  const res = await rtcApi(token, `/sessions/${sid}`, {
+    method: 'GET',
+  })
+  return await res.json() as SessionStat
+}
+
+interface MediaSdp {
+  m: string
+  sendOrRecv: string
+  mid: string
+  ice: string[]
+  content: string[]
+}
+
+function disableMedia(media: MediaSdp): MediaSdp {
+  if (!media.m)
+    return media
+
+  const newContent = []
+  for (const line of media.content) {
+    if (line.startsWith('a=extmap')) {
+      continue
+    }
+    if (line.startsWith('a=rtpmap')) {
+      continue
+    }
+    if (line.startsWith('a=rtcp-fb')) {
+      continue
+    }
+    if (line.startsWith('a=fmtp')) {
+      continue
+    }
+    newContent.push(line)
+  }
+
+  if (media.m.startsWith('m=video')) {
+    media.m = 'm=video 9 UDP/TLS/RTP/SAVPF 96'
+    newContent.push('a=rtpmap:96 VP8/90000')
+  }
+  if (media.m.startsWith('m=audio')) {
+    media.m = 'm=audio 0 UDP/TLS/RTP/SAVPF 111'
+    newContent.push('a=rtpmap:111 opus/48000/2')
+  }
+  media.ice = []
+  media.sendOrRecv = 'a=recvonly'
+  media.content = newContent
+
+  return media
+}
+
+// @ts-expect-error keep
+function _getMediaFromSdp(sfuOffer: SessionDescription): MediaSdp[] {
+  const ret: MediaSdp[] = []
+  const lines = sfuOffer.sdp.split('\r\n')
+
+  let activeMedia = null
+  for (const line of lines) {
+    if (line.length == 0) {
+      continue
+    }
+    if (line.startsWith('m=')) {
+      activeMedia = {
+        m: line,
+        mid: '',
+        sendOrRecv: '',
+        ice: [],
+        content: [],
+      } as MediaSdp
+      ret.push(activeMedia)
+      continue
+    }
+    if (!activeMedia) {
+      continue
+    }
+    if (line.startsWith('a=candidate:') || line.startsWith('a=ice-') || line.startsWith('a=fingerprint')) {
+      activeMedia.ice.push(line)
+      continue
+    }
+    if (line.startsWith('a=mid')) {
+      activeMedia.mid = line
+      continue
+    }
+    if (line.startsWith('a=send') || line.startsWith('a=recv')) {
+      activeMedia.sendOrRecv = line
+      continue
+    }
+    activeMedia.content.push(line)
+  }
+
+  return ret
+}
+
+// @ts-expect-error keep
+function _createAnswerForPlayer(sfuMedia: MediaSdp[], playerMedia: MediaSdp[], baseOffer: SessionDescription): SessionDescription {
+  const finalMedia: MediaSdp[] = []
+
+  let validAudio: MediaSdp | null = null
+  let validVideo: MediaSdp | null = null
+  const validCandidate: string[] = []
+  sfuMedia.forEach(media => {
+    const valid = media.sendOrRecv.startsWith('a=send')
+    if (valid) {
+      if (media.m.startsWith('m=video'))
+        validVideo = media
+      if (media.m.startsWith('m=audio'))
+        validAudio = media
+    }
+    media.ice.forEach(ice => {
+      if (ice.startsWith('a=candidate'))
+        validCandidate.push(ice)
+    })
+  })
+
+  for (const media of playerMedia) {
+    if (media.m.startsWith('m=video')) {
+      if (validVideo) {
+        const m = validVideo as MediaSdp
+        m.mid = media.mid
+        finalMedia.push(m)
+      } else {
+        finalMedia.push(disableMedia(media))
+      }
+    }
+    if (media.m.startsWith('m=audio')) {
+      if (validAudio) {
+        const m = validAudio as MediaSdp
+        m.mid = media.mid
+        finalMedia.push(validAudio)
+      } else {
+        finalMedia.push(disableMedia(media))
+      }
+    }
+  }
+  
+  const answerLines: string[] = []
+  const lines = baseOffer.sdp.split('\r\n')
+  for (const line of lines) {
+    if (line.startsWith('a=group:BUNDLE')) {
+      const mids = finalMedia
+        .map(m => m.mid.replace('a=mid:', '').trim())
+        .filter(mid => mid !== '')
+        .join(' ')
+      answerLines.push(`a=group:BUNDLE ${mids}`)
+      continue
+    }
+    if (line.startsWith('m=')) {
+      break
+    }
+    answerLines.push(line)
+  }
+
+  console.log('player', playerMedia)
+  console.log('a', validAudio)
+  console.log('v', validVideo)
+  console.log('final', finalMedia)
+  console.log('ans1', answerLines)
+
+  let hasCandidate = false
+  for (const media of finalMedia) {
+    answerLines.push(media.m)
+    media.content.forEach(content => {
+      if (content.startsWith('a=setup')) {
+        answerLines.push('a=setup:passive')
+      } else {
+        answerLines.push(content)
+      }
+    })
+    media.ice.forEach(ice => {
+      if (ice.startsWith('a=candidate')) {
+        hasCandidate = true
+      }
+      answerLines.push(ice)
+    })
+    if (!hasCandidate) {
+      validCandidate.forEach(cand => {
+        answerLines.push(cand)
+      })
+      answerLines.push('a=end-of-candidates')
+      hasCandidate = true
+    }
+
+    answerLines.push(media.mid)
+    answerLines.push(media.sendOrRecv)
+  }
+  console.log('ans2', answerLines)
+
+  return {
+    type: 'answer',
+    sdp: answerLines.join('\r\n') + '\r\n',
+  }
+}
+
+// @ts-expect-error keep
+function _createAnswerForSfu(sfuMedia: MediaSdp[], playerMedia: MediaSdp[], baseOffer: SessionDescription): SessionDescription {
+  const finalMedia: MediaSdp[] = []
+
+  let validAudio: MediaSdp | null = null
+  let validVideo: MediaSdp | null = null
+  const validCandidate: string[] = []
+  playerMedia.forEach(media => {
+    const valid = media.sendOrRecv.startsWith('a=send')
+    if (valid) {
+      if (media.m.startsWith('m=video'))
+        validVideo = media
+      if (media.m.startsWith('m=audio'))
+        validAudio = media
+    }
+    media.ice.forEach(ice => {
+      if (ice.startsWith('a=candidate'))
+        validCandidate.push(ice)
+    })
+  })
+
+  for (const media of sfuMedia) {
+    if (media.m.startsWith('m=video')) {
+      if (validVideo) {
+        const m = validVideo as MediaSdp
+        m.mid = media.mid
+        finalMedia.push(m)
+      } else {
+        finalMedia.push(disableMedia(media))
+      }
+    }
+    if (media.m.startsWith('m=audio')) {
+      if (validAudio) {
+        const m = validAudio as MediaSdp
+        m.mid = media.mid
+        finalMedia.push(validAudio)
+      } else {
+        finalMedia.push(disableMedia(media))
+      }
+    }
+  }
+  
+  const answerLines: string[] = []
+  const lines = baseOffer.sdp.split('\r\n')
+  for (const line of lines) {
+    if (line.startsWith('a=group:BUNDLE')) {
+      const mids = finalMedia
+        .map(m => m.mid.replace('a=mid:', '').trim())
+        .filter(mid => mid !== '')
+        .join(' ')
+      answerLines.push(`a=group:BUNDLE ${mids}`)
+      continue
+    }
+    if (line.startsWith('m=')) {
+      break
+    }
+    answerLines.push(line)
+  }
+
+  console.log('a', validAudio)
+  console.log('v', validVideo)
+  console.log('final', finalMedia)
+  console.log('ans1', answerLines)
+
+  let hasCandidate = false
+  for (const media of finalMedia) {
+    answerLines.push(media.m)
+    media.content.forEach(content => {
+      if (content.startsWith('a=setup')) {
+        answerLines.push('a=setup:passive')
+      } else {
+        answerLines.push(content)
+      }
+    })
+    media.ice.forEach(ice => {
+      if (ice.startsWith('a=candidate')) {
+        hasCandidate = true
+      }
+      answerLines.push(ice)
+    })
+    if (!hasCandidate) {
+      validCandidate.forEach(cand => {
+        answerLines.push(cand)
+      })
+      answerLines.push('a=end-of-candidates')
+      hasCandidate = true
+    }
+
+    answerLines.push(media.mid)
+    answerLines.push(media.sendOrRecv)
+  }
+  console.log('ans2', answerLines)
+
+  return {
+    type: 'answer',
+    sdp: answerLines.join('\r\n') + '\r\n',
+  }
+}
 
 export default app
