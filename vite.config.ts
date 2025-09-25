f5326d97f27e1933ae5eb675846e6dbe3a8ac78f
lc <lichon@foxmail.com>
2025-09-25 15:53:22 +0800

    remove mock test

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
-import { viteMockServe } from 'vite-plugin-mock';
 
 export default defineConfig({
   plugins: [
     react(),
     cloudflare(),
-    viteMockServe({
-      mockPath: 'mock',
-      watchFiles: true,
-    }),
     {
       name: 'vite-custom-ws-cmd',
       configureServer(server) {
